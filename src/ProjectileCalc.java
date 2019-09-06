d4391f47929f3274b4712e3fa4fe026e73a6f4a9
PingPonguin <wilsonw62@gmail.com>
2019-09-06 19:08:43 -0400

    Testing Commit: real initial commit

---
+++ src/ProjectileCalc.java
@@ -0,0 +1,20 @@
+public class ProjectileCalc {
+
+    /*This is a calculator for projectiles. Specifically, the user inputs the speed of the projectile and the distance
+    *to target, and the calculator outputs the time to target and how high to aim.*/
+
+    public static void main(String []args) {
+
+        System.out.println("This is a ballistics calculator. Input the speed of the projectile and distance to target to know time to target and how high to aim.");
+
+
+
+
+
+
+
+
+    }
+
+
+}
