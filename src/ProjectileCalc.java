f70fb0f416c78501b681ac510f85b272e928e78e
Wilson W <wilsonw62@gmail.com>
2019-09-06 22:25:04 -0400

    Added initilization for variables of user input

--- src/ProjectileCalc.java
+++ src/ProjectileCalc.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ProjectileCalc {
 
     /*This is a calculator for projectiles. Specifically, the user inputs the speed of the projectile and the distance
@@ -6,8 +8,19 @@ public class ProjectileCalc {
     public static void main(String []args) {
 
         System.out.println("This is a ballistics calculator. Input the speed of the projectile and distance to target to know time to target and how high to aim.");
-        //Welcome message?//
-        //Hello from laptop!//
+
+        //initialize variables for user's input
+        int projspeed;
+        int range;
+
+        //Initialize? scanner and set up input for user
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Projectile Speed:");
+        projspeed = scan.nextInt();
+        System.out.println("Range to Target:");
+        range = scan.nextInt();
+
+
 
 
 
